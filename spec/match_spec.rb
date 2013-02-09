03bf5513123ce14a99820749ebb5cb0fb72947bf
edchiou <eddy3334@gmail.com>
2013-02-08 23:30:55 -0800

    Added several methods to match.rb and tests to match_spec.rb

--- spec/match_spec.rb
+++ spec/match_spec.rb
@@ -12,6 +12,10 @@ describe Dota::Match do
     match.id.must_equal 27110133
   end
 
+  it 'returns match sequence number' do
+    match.seq_num.must_equal 27106670
+  end
+
   it 'returns start time' do
     match.start.must_equal Time.at(1342739723)
   end
@@ -27,5 +31,37 @@ describe Dota::Match do
   it 'returns players' do
     match.players.must_be_kind_of Array
     match.players.first.must_be_kind_of Dota::Player
+  end
+
+  it 'returns season' do
+      match.season.must_equal 7
+  end
+
+  it 'returns duration' do
+      match.duration.must_equal 1914
+  end
+
+  it 'returns number of human players' do
+      match.human_players.must_equal 10
+  end
+
+  it 'returns number of positive votes' do
+      match.positive_votes.must_equal 0
+  end
+
+  it 'returns number of negative votes' do
+      match.negative_votes.must_equal 0
+  end
+
+  it 'returns cluster' do
+      match.cluster.must_equal 131
+  end
+
+  it 'returns game_mode' do
+      match.game_mode.must_equal 'All Pick'
+  end
+
+  it 'returns lobby type' do
+      match.lobby_type.must_equal 'Public matchmaking'
   end
 end
