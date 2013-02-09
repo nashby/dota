d4284b1c68efaa22b1aabb54eacd3cd501a53747
Vasiliy Ermolovich <younash@gmail.com>
2013-02-09 15:52:25 +0300

    delegate leagues and live_leagues to the client

--- spec/delegation_spec.rb
+++ spec/delegation_spec.rb
@@ -1,12 +1,33 @@
 require 'spec_helper'
 
 describe 'delegation' do
+  let(:mock) { MiniTest::Mock.new }
+
   it 'delegates #match to the client' do
-    mock = MiniTest::Mock.new
     mock.expect(:match, :result, [123])
 
     Dota.stub(:client, mock) do
       Dota.match(123).must_equal :result
+    end
+
+    mock.verify
+  end
+
+  it 'delegates #leagues to the client' do
+    mock.expect(:leagues, :result, [])
+
+    Dota.stub(:client, mock) do
+      Dota.leagues.must_equal :result
+    end
+
+    mock.verify
+  end
+
+  it 'delegates #live_leagues to the client' do
+    mock.expect(:live_leagues, :result, [])
+
+    Dota.stub(:client, mock) do
+      Dota.live_leagues.must_equal :result
     end
 
     mock.verify
